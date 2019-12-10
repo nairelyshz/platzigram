aa212831b4749a89bf4a1b0897aaf39a69ab3aed
nairelyhz <njhs694@gmail.com>
2019-12-09 20:23:21 -0400

    login

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -20,12 +20,13 @@ from django.urls import path
 
 from platzigram import views as local_views
 from posts import views as posts_views
-
+from users import views as users_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('helloworld',local_views.helloworld),
     path('hi',local_views.hi),
     path('hi/<str:name>/<int:age>/',local_views.say_hi),
-    path('posts/',posts_views.list_posts)
+    path('posts/',posts_views.list_posts,name='feed'),
+    path('users/login/',users_views.login_view, name='login')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
