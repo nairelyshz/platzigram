7e3bdd9cfaf8ad2b3afcc143888403dbd6472b9d
nairelyhz <njhs694@gmail.com>
2019-12-11 15:24:40 -0400

    pending fix url de cada app

--- platzigram/middleware.py
+++ platzigram/middleware.py
@@ -19,8 +19,8 @@ class ProfileCompletionMiddleware:
         if not request.user.is_anonymous:
             profile = request.user.profile
             if not profile.picture or not profile.biography:
-                if request.path not in [reverse('update_profile'), reverse('logout')]:
-                    return redirect('update_profile')
+                if request.path not in [reverse('update'), reverse('logout')]:
+                    return redirect('update')
 
         response = self.get_response(request)
         return response
