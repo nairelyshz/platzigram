8fe184700a75af1810f07ddf5d66fd7f9ba4aa19
nairelyhz <njhs694@gmail.com>
2019-12-11 16:07:41 -0400

    deatils done

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 
 from posts import views as posts_views
 from users import views as users_views
-
+from django.views.generic import TemplateView
 urlpatterns = [
 
     path('admin/', admin.site.urls),
@@ -17,6 +17,9 @@ urlpatterns = [
     path('users/logout/', users_views.logout_view, name='logout'),
     path('users/signup/', users_views.signup, name='signup'),
     path('users/me/profile/', users_views.update_profile, name='update'),
+    path('users/<str:username>/',  view=TemplateView.as_view(template_name='users/detail.html'),
+        name='detail')
+   
     # path('posts/', include(('posts.urls', 'posts'), namespace='posts')),
     # path('users/', include(('users.urls', 'users'), namespace='users')),
 
--- users/views.py
+++ users/views.py
@@ -41,8 +41,8 @@ def update_profile(request):
             profile.biography = data['biography']
             profile.picture = data['picture']
             profile.save()
-
-            return redirect('update')
+            url = reverse('detail', kwargs={'username': request.user.username})
+            return redirect(url)
 
     else:
         form = ProfileForm()
